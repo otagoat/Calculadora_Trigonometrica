678009b9d0e88095a68c5e4446b258cfa2a2682a
otagoat <otagoat14@gmail.com>
2025-06-13 19:59:01 -0600

    Definicion de variables y estructuras

--- Main.java
+++ Main.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
 
-    System.out.println("Hola mundo");
+            System.out.print("Función (sin, cos, tan, arcsin, arccos, arctan, sinh, cosh, tanh): ");
+            String funcion = sc.nextLine();
 
+            System.out.print("Ángulo α (en grados): ");
+            double angulo = sc.nextDouble();
+
+            
+            double resultado = switch (funcion.toLowerCase()){}
+
+        }
     }
 }
